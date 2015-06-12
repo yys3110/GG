0836899ac62a7c0986028cbe7d6b64252ae61748
김동은 <vega1423@gmail.com>
2015-06-12 16:53:38 +0900

    동은 성검 신체

---
+++ Assets/dongeun/mon-Divine Sword/divinesword_active.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class divinesword_active : MonoBehaviour {
+	public GameObject target;
+	public int turn_cooltime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = transform.parent.GetComponent<monster>().target;
+		if(play_system.turn == 2){
+			target.GetComponent<player>().HP_system(target.GetComponent<player>().hp_max/2,false,transform.parent.gameObject);
+			Destroy(gameObject);
+			transform.parent.gameObject.GetComponent<monster>().wait_();
+		}
+	}
+}
--- Assets/dongeun/mon-God`s Body/Godsbody_active.cs
+++ Assets/dongeun/mon-God`s Body/Godsbody_active.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class Godsbody_active : MonoBehaviour {
+	int count = 0;
+	int MAX_count = 0;
+	public int passive_turn = 0;
 
+	int defense_ = 0; // 초기 방어값 저장
 	// Use this for initialization
 	void Start () {
+		count = play_system.game_turn+1;
+		MAX_count = count + passive_turn;
+		Debug.Log ("count" + count);
+		Debug.Log ("MAX_count" + MAX_count);
+		defense_ = transform.parent.gameObject.GetComponent<monster>().defense;
+		transform.parent.gameObject.GetComponent<monster>().defense = 9999;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if(count == play_system.game_turn){
+			count ++;
+
+			if(count == MAX_count){
+				transform.parent.gameObject.GetComponent<monster>().defense = defense_;
+				Destroy(gameObject);
+			}
+
+		}
 	}
 }
